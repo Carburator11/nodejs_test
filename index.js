9d030228eeaf5493c924aa4aa4f2957789b214cc
Pierre Vautherin <pierre.vautherin@sfrpims.fr>
2018-10-10 10:16:04 +0200

    Server + babel

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-require("@babel/polyfill");
+const express = require('express');
+const app = express()
 
+app.get('/', (req, res) => {
+    console.log("hello")
+    res.json({response: "hello"})
+} )
 
-
-console.log("hello")
+app.listen(3000, ()=> console.log("Listening on port 3000"))
