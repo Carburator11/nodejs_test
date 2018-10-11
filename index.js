e4af1f959044b37a735335afa308ad0bd8d52f97
Pierre Vautherin <pierre.vautherin@sfrpims.fr>
2018-10-11 09:51:54 +0200

    IP location

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const express = require('express');
+// no semi-colon styling^^
+
+const iplocation =require('iplocation')
+const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
     console.log("hello")
-    res.json({response: "hello"})
+    const ip =request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+    console.log("IP: ", ip, " - ", request.ip)
+    iplocation(ip, (error, res) => {
+        //const {lat, lon, city} = res
+        console.log("Iplocation result: ", res)
+        // console.log(`Ip adress ${ip} located in ${city || "<not found>"} (${lat}, ${lon}) `)
+        response.json(
+            {'response': `hello, your IP address is ${ip}, your location is ${"<not found>"}`,
+            'iplocation': res
+        })
+    })
 } )
 
 app.listen(3000, ()=> console.log("Listening on port 3000"))
