14a5152428516a30e0b559cc0fd291f1ec82d5b7
Pierre Vautherin <pierre.vautherin@sfrpims.fr>
2018-10-11 11:50:23 +0200

    JSON handling

--- index.js
+++ index.js
@@ -11,41 +11,61 @@ app.get('/', (request, response) => {
     const ip = ipFromHeader ? ipFromHeader.split(',')[0] : request.ip
     console.log("IP: ", ip)
 
-    iplocation(ip, (error, res) => {
+    iplocation(ip, (err, res) => {
         const {latitude, longitude, city} = res
         console.log("Iplocation result: ", res)
         
         // it's just a test project!
         const apiKey = "e9b13ab6140094e90af27864db89a7b1"
-        const url = `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${apiKey}`
-        console.log(url)
-        if(latitude && longitude){
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${apiKey}`
+        const testUrl = "http://api.openweathermap.org/data/2.5/weather?lat=48.6075&lon=7.7493&APPID=e9b13ab6140094e90af27864db89a7b1"
 
-            fetch(url)
-                .then(
-                    apiResponse => {
-                        response.json(
-                            {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
-                            'iplocation': res,
-                            'apiResponse': apiResponse
-                        })
-                    }
-                )
-                .catch(
+        if(latitude && longitude){
+            console.log(url)
+            fetch(url, {method: 'get'})
+            .then(
+                apiResponse => {
+                    return apiResponse.json()
+                }).then(json => {
                     response.json(
                         {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
                         'iplocation': res,
-                        'apiResponse': "Weather API error"
+                        'apiResponse': json
+                    })
+                })
+            .catch( error=>{
+                console.log("error", error)
+                    response.json(
+                        {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
+                        'iplocation': res,
+                        'apiResponse': "Weather API error",
+                        'error': error
                         }
                     )
-                )
+                })
         } else {
-            response.json(
-                {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
-                'iplocation': res,
-                'apiResponse': "IP location error"
-                }
-            )
+            fetch(testUrl)
+                .then(
+                    apiResponse => {
+                        return apiResponse.json()
+                    }).then(json => {
+                        response.json(
+                            {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
+                            'iplocation': res,
+                            'apiResponse': json
+                        })
+                    })
+                .catch( error=>{
+                    console.log("error", error)
+                    response.json(
+                        {'response': `hello, your IP address is ${ip}, your location is ${city || "<not found>"}`,
+                        'iplocation': res,
+                        'apiResponse': "(Test URL)Weather API error",
+                        'Test URL': testUrl,
+                        'error': error
+                        }
+                    )
+                })
         }
     })
 })
